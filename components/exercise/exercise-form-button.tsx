2bf1ac3f2526198e5a7214e43a74e27c6603808d
fivaz <31249205+fivaz@users.noreply.github.com>
2026-01-12 10:13:59 +0100

    feat: implement update and delete exercise

--- components/exercise/exercise-form-button.tsx
+++ components/exercise/exercise-form-button.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { useState } from "react";
 
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, Trash2Icon } from "lucide-react";
 
 import { ExerciseForm } from "@/components/exercise/exercise-form";
 import { Button } from "@/components/ui/button";
@@ -14,6 +14,9 @@ import {
 	DrawerTitle,
 	DrawerTrigger,
 } from "@/components/ui/drawer";
+import { useConfirm } from "@/hooks/confirm/use-confirm";
+import { useExercises } from "@/hooks/exercise/exercises-store-context";
+import { ROUTES } from "@/lib/consts";
 import { buildEmptyExercise, ExerciseUI } from "@/lib/exercise/type";
 
 type ExerciseFormButtonProps = React.ComponentProps<typeof Button> & {
@@ -31,11 +34,23 @@ export function ExerciseFormButton({
 }: ExerciseFormButtonProps) {
 	// Internal state management if external state isn't provided
 	const [internalOpen, setInternalOpen] = useState(false);
-
+	const { deleteItem } = useExercises();
+	const confirm = useConfirm();
 	// Determine which state to use
 	const isControlled = externalOpen !== undefined;
 	const open = isControlled ? externalOpen : internalOpen;
 	const setOpen = isControlled ? externalOnOpenChange : setInternalOpen;
+
+	const handleDelete = async () => {
+		const confirmed = await confirm({
+			title: "Delete Exercise",
+			message: `Are you sure you want to delete "${exercise.name}"? This action cannot be undone`,
+		});
+
+		if (!confirmed) return;
+
+		deleteItem(exercise.id);
+	};
 
 	return (
 		<Drawer open={open} onOpenChange={setOpen}>
@@ -54,8 +69,17 @@ export function ExerciseFormButton({
 
 			<DrawerContent className="max-h-[90vh]">
 				<div className="mx-auto w-full max-w-md overflow-y-auto pb-6">
-					<DrawerHeader>
+					<DrawerHeader className="relative">
 						<DrawerTitle>{exercise.id ? "Edit Exercise" : "Create Exercise"}</DrawerTitle>
+						{exercise.id && (
+							<Button
+								variant="destructive"
+								className="absolute top-0 right-0 mx-4 my-2"
+								onClick={handleDelete}
+							>
+								<Trash2Icon />
+							</Button>
+						)}
 					</DrawerHeader>
 
 					<ExerciseForm exercise={exercise} onClose={() => setOpen?.(false)} />
