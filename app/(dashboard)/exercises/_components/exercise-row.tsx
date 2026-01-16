c0f559a513d3c02ba5cbc86abf4b832acbe23773
fivaz <31249205+fivaz@users.noreply.github.com>
2026-01-17 00:04:50 +0100

    feat: update design of exercise row

--- app/(dashboard)/exercises/_components/exercise-row.tsx
+++ app/(dashboard)/exercises/_components/exercise-row.tsx
@@ -21,8 +21,8 @@ export function ExerciseRow({ exercise }: ExerciseRowProps) {
 					alt={exercise.name}
 					className="h-full w-full object-cover transition-transform group-hover:scale-105"
 				/>
-				<div className="absolute inset-0 flex flex-col justify-end bg-linear-to-t from-black/80 via-transparent to-transparent p-4">
-					<h3 className="text-lg font-bold text-white">{exercise.name}</h3>
+				<div className="absolute inset-0 flex flex-col justify-end bg-linear-to-t from-black/90 to-black/20 p-4">
+					<h3 className="text-lg font-bold text-white capitalize">{exercise.name}</h3>
 					<p className="text-sm text-white">{exercise.muscles.join(", ")}</p>
 				</div>
 			</button>
